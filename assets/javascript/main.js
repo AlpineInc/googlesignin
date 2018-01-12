a5de6e74f9b5f1baef3dba1ceef7c9353f0ad91b
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-12 13:36:22 -0600

    temp

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,26 @@
 $(function() {
-    var giphyCategory = ["pets", "football", "bootcamp"];
+    var googleAuth;
+    var googleUser;
 
-    function onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    gapi.load("auth2", function() {
+        googleAuth = gapi.auth2.init({
+            client_id: "245751156594-tp337vitvvl9ltm4jhpoirm249v20tsf.apps.googleusercontent.com",
+            scope: "profile"
+        });
+        attachSignin($("#btn-signIn"));
+    });
+
+
+    function attachSignin(element) {
+        console.log(element.id);
+        auth2.attachClickHandler(element, {},
+            function(googleUser) {
+                document.getElementById('name').innerText = "Signed in: " +
+                    googleUser.getBasicProfile().getName();
+            },
+            function(error) {
+                alert(JSON.stringify(error, undefined, 2));
+            });
     }
 
 });
