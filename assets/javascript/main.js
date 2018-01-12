6737e4c794cdecb6a065094af441e7f97b937358
Ravish Rao <Adkaravish_Rao@ad.infosys.com>
2018-01-12 13:41:34 -0600

    temp

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,26 +1,23 @@
-$(function() {
-    var googleAuth;
-    var googleUser;
+var googleAuth;
+var googleUser;
 
-    gapi.load("auth2", function() {
-        googleAuth = gapi.auth2.init({
-            client_id: "245751156594-tp337vitvvl9ltm4jhpoirm249v20tsf.apps.googleusercontent.com",
-            scope: "profile"
-        });
-        attachSignin($("#btn-signIn"));
+gapi.load("auth2", function() {
+    googleAuth = gapi.auth2.init({
+        client_id: "245751156594-tp337vitvvl9ltm4jhpoirm249v20tsf.apps.googleusercontent.com",
+        scope: "profile"
     });
-
-
-    function attachSignin(element) {
-        console.log(element.id);
-        auth2.attachClickHandler(element, {},
-            function(googleUser) {
-                document.getElementById('name').innerText = "Signed in: " +
-                    googleUser.getBasicProfile().getName();
-            },
-            function(error) {
-                alert(JSON.stringify(error, undefined, 2));
-            });
-    }
-
+    attachSignin($("#btn-signIn"));
 });
+
+
+function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+            document.getElementById('name').innerText = "Signed in: " +
+                googleUser.getBasicProfile().getName();
+        },
+        function(error) {
+            alert(JSON.stringify(error, undefined, 2));
+        });
+}
